4b3516e8b97a5e7b29a155391bd4b6f065901adf
Brian Takita <brian.takita@gmail.com>
2023-10-15 21:14:06 -0400

    .gitmodules--https--replace.sh:
    	+ git submodule foreach

--- .gitmodules--https--replace.sh
+++ .gitmodules--https--replace.sh
@@ -1,1 +1,2 @@
 sed -i 's/:/\//g;s/git@/https:\/\//g' .gitmodules
+git submodule foreach 'git config --local url."git@github.com:$(basename $(pwd))".insteadOf "https://github.com/$(basename $(pwd))"'
