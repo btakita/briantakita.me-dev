c34cfedad1b44fb166cc5a4762cc58bfae77ef95
Brian Takita <brian.takita@gmail.com>
2023-11-29 22:58:35 -0500

    @btakita/briantakita.me:
    	.vercel--build.sh: touch bun.lockb
    	/linkument: stub content

--- .vercel--build.sh
+++ .vercel--build.sh
@@ -2,3 +2,4 @@
 cd app/briantakita.me
 pwd
 bun -b run build
+(cd .vercel/output/functions/render.func && touch bun.lockb)
--- .vercel--install.sh
+++ .vercel--install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm -rf ./app/briantakita.me
 ./.gitmodules--https.sh
 git submodule update --init --recursive
 bun install
