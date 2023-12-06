98658f378d6092452ee0cf54b3e5092ce9a7238f
Brian Takita <brian.takita@gmail.com>
2023-12-06 05:35:44 -0500

    .vercel--install.sh: rm all submodule directories to clean up repo: then reapply https submodules

--- .vercel--install.sh
+++ .vercel--install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 DIR=$(dirname $0)
 (cd $DIR && ./.gitmodules--https.sh)
+rm -rf $DIR/app/assets.briantakita.me
 rm -rf $DIR/app/briantakita.me
+rm -rf $DIR/lib/*
 (cd $DIR && git submodule update --init --recursive)
 (cd $DIR && bun install)
